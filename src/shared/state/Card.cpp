80936d88f01d73deb3e55f46b91fb8d8e0ef2ed5
Onur <xblaze37@gmail.com>
2023-10-17 16:30:50 +0200

    **feature: Card constructor**
    __Commit Description:__
    
    Constructors of Card class
    
    [CR #008]

--- src/shared/state/Card.cpp
+++ src/shared/state/Card.cpp
@@ -3,12 +3,20 @@
 namespace state {
 
     /// @brief Create an empty card
-    Card::Card()
+    Card::Card() :
+    name(""),
+    productionGain({}),
+    design(sf::Texture()),
+    victoryPoints({})
     {
     }
     /// @brief Create a card
     Card::Card(std::string name, std::vector<ResourceToProduce*> productionGain, sf::Texture design, std::vector<CardVictoryPoint*> victoryPoints)
     {
+        this->name = name;
+        this->productionGain = productionGain;
+        this->design = design;
+        this->victoryPoints = victoryPoints;
     }
 
     /// @brief Card destructor
