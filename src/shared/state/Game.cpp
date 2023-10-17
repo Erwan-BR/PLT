3a89140b074abe8c098d34d720a25c4784e306a0
Erwan <119836222+Erwan-BR@users.noreply.github.com>
2023-10-17 15:47:35 +0200

    **refactor: Update syntax**
    _Commit description_
    
    Update syntax of game.
    
    [CR #008]

--- src/shared/state/Game.cpp
+++ src/shared/state/Game.cpp
@@ -1,18 +1,19 @@
 //Written by Adrien MILLASSEAU
 
 #include "Game.h"
-namespace state{
 
-	///@brief : Create an instance of the class Game
-	Game::Game(){
-		this->turn = 0;
-		this->phase = DRAFT;
+namespace state {
+	///@brief Create an instance of the class Game
+	Game::Game() :
+	turn(0),
+	phase(DRAFT)
+	{
 		this->isClockwise = true;
 	}
 
-	///@brief : Create an instance of the class Game with players specified
-	///@param players : Vector of pointers which designate the players of the game
-	Game::Game(std::vector<Player*> players):
+	///@brief Create an instance of the class Game with players specified
+	///@param players Vector of pointers which designate the players of the game
+	Game::Game(std::vector<Player*> players) :
 		players(players)
 	{
 		this->turn = 0;
@@ -20,77 +21,91 @@ namespace state{
 		this->isClockwise = true;
 	}
 
-	///@brief : Destructor of the Class Game
-	Game::~Game (){
+	///@brief Destructor of the Class Game
+	Game::~Game ()
+	{
 
 	}
 
-	///@brief : Initialize the game
-	void Game::initGame (){
+	///@brief Initialize the game
+	void Game::initGame ()
+	{
 
 	}
 
-	///@brief : Create and Initialize the Cards for the game
-	void Game::initCards (){
+	///@brief Create and Initialize the Cards for the game
+	void Game::initCards ()
+	{
 
 	}
 
-	///@brief : Create and Initialize the Empire for the game
-	void Game::initEmpire (){
+	///@brief Create and Initialize the Empire for the game
+	void Game::initEmpire ()
+	{
 
 	}
 
-	///@brief : Start the game
-	void Game::startGame (){
+	///@brief Start the game
+	void Game::startGame ()
+	{
 
 	}
 
-	///@brief : Manage the progress of the game and start the next phase among Draft, Planification and Production
-	void Game::nextPhase (){
+	///@brief Manage the progress of the game and start the next phase among Draft, Planification and Production
+	void Game::nextPhase ()
+	{
 
 	}
 
-	///@brief : Start one of the four turn of the game
-	void Game::newTurn (){
+	///@brief Start one of the four turn of the game
+	void Game::newTurn ()
+	{
 
 	}
 
-	///@brief : Initialize the Draft part of the game during which players select their cards
-	void Game::initDraft (){
+	///@brief Initialize the Draft part of the game during which players select their cards
+	void Game::initDraft ()
+	{
 
 	}
 
-	///@brief : Start a Draft
-	void Game::nextDraft (){
+	///@brief Start a Draft
+	void Game::nextDraft ()
+	{
 
 	}
 
-	///@brief : End the current Draft phase
-	void Game::endDraft (){
+	///@brief End the current Draft phase
+	void Game::endDraft ()
+	{
 
 	}
 
-	///@brief : Initialize the Planification phase during which players choose the cards they will try to build
-	void Game::initPlanification (){
+	///@brief Initialize the Planification phase during which players choose the cards they will try to build
+	void Game::initPlanification ()
+	{
 
 	}
 
-	///@brief : Manage the phase of production for all player and one resource
-	///@param toProduceResource : Pointer which designate the type of resource currently to produce
-	void Game::produceResource (Resource* toProduceResource){
+	///@brief Manage the phase of production for all player and one resource
+	///@param toProduceResource Pointer which designate the type of resource currently to produce
+	void Game::produceResource (Resource* toProduceResource)
+	{
 
 	}
 
-	///@brief : Send a resource earned by a player to let him use it
-	///@param resource : Pointer which designate the type of resource sent
-	///@param player : Pointer which designate the player who receive the resource
-	void Game::sendResourceToPlayer (Resource* resource, Player* player){
+	///@brief Send a resource earned by a player to let him use it
+	///@param resource Pointer which designate the type of resource sent
+	///@param player Pointer which designate the player who receive the resource
+	void Game::sendResourceToPlayer (Resource* resource, Player* player)
+	{
 
 	}
 
-	///@brief : Debug method to check the state of the instance of Game
-	///@return : String explaining the state of Game
-	std::string toString (){
+	///@brief Debug method to check the state of the instance of Game
+	///@return String explaining the state of Game
+	std::string toString ()
+	{
 		return "Game";
 	}
 
