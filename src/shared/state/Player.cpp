2be7ce60414a10615d11ca7ffb99a325b6b01329
Delatouche Maxime <delatouche.maxime.other@gmail.com>
2023-10-17 15:58:14 +0200

    **feature : Add comment**
    __Commit Description :__
    
    Added missing comments
    
    [CR #008]

--- src/shared/state/Player.cpp
+++ src/shared/state/Player.cpp
@@ -28,44 +28,44 @@ namespace state {
     }
 
     /// @brief Construct the card "cardToBuild"
-    /// @param cardToBuild 
+    /// @param cardToBuild : the card to build
     void Player::construct(DevelopmentCard* cardToBuid)
     {
 
     }
 
     /// @brief Add a resource "resource" to the card "card"
-    /// @param resource
-    /// @param card
+    /// @param resource : resource placed on the card "card"
+    /// @param card : card that will receive the resource "resource"
     void Player::addResource(Resource* ressource, DevelopmentCard* card)
     {
 
     }
 
     /// @brief Discard the card "toDiscardCard"
-    /// @param toDiscardCard
+    /// @param toDiscardCard : card that is going to be discarded
     void Player::discardCard(DevelopmentCard* toDiscardCard)
     {
 
     }
 
     /// @brief Calculate the quantity of resource named "resourceToProduce" produced by the player
-    /// @param resourceToProduce
-    /// @return
+    /// @param resourceToProduce : type of the resource that will be produced
+    /// @return 0 : quantity of resource "resourceToProduce" obtained by the player
     int Player::computeProduction(Resource* resourceToProduce)
     {
         return 0;
     }
 
     /// @brief Calculate the number of victory points the player has
-    /// @return
+    /// @return 0 : number of victory points the player on the moment
     int Player::computeVictoryPoint()
     {
         return 0;
     }
 
     /// @brief Takes a resource produced by the player and places it on the Empire of the player
-    /// @param resource
+    /// @param resource : type of resource that one will be send to the empire card of the player
     void Player::sendResourceToEmpire(Resource* resource)
     {
 
@@ -78,14 +78,14 @@ namespace state {
     }
 
     /// @brief Add the selected card "card" from the drafting deck to the drafted one
-    /// @param card 
+    /// @param card : card choosed by the player
     void Player::chooseDraftCard(DevelopmentCard* card)
     {
 
     }
 
     /// @brief Return the selected token by the player (Colonel/General)
-    /// @return
+    /// @return true : boolean returned by the choice of the player (Colonel => true, General => false)
     bool Player::chooseColonelToken()
     {
         return true;
