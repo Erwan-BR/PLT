14e9743873a475a7660f6ebe52cbde8272576abb
Erwan <cuir-saunas.07@icloud.com>
2023-10-20 13:46:40 +0200

    **feature: Correcting Dia for EmpireCard constructor**
    _Commit Description:_
    
    - Deleting a pointer to a enum on the constructor of EmpireCard.
    - Correcting the constructor in EmpireCard.cpp associated to this error.
    
    [CR #011]

--- src/shared/state/EmpireCard.cpp
+++ src/shared/state/EmpireCard.cpp
@@ -20,7 +20,7 @@ namespace state {
     /// @param productionGainAdvanced Advanced production provided by the empire
     /// @param victoryPointsAdvanced Advanced victory points provided by the empire
     /// @param empire Region the empire belongs to
-    EmpireCard::EmpireCard(std::string name, std::vector<ResourceToProduce*> productionGain, sf::Texture design, std::vector<CardVictoryPoint*> victoryPoints, std::vector<ResourceToProduce*> productionGainAdvanced, std::vector<CardVictoryPoint*> victoryPointsAdvanced, EmpireLand* empire) :
+    EmpireCard::EmpireCard(std::string name, std::vector<ResourceToProduce*> productionGain, sf::Texture design, std::vector<CardVictoryPoint*> victoryPoints, std::vector<ResourceToProduce*> productionGainAdvanced, std::vector<CardVictoryPoint*> victoryPointsAdvanced, EmpireLand empire) :
     Card(name, productionGain, design, victoryPoints),
     productionGainAdvanced(productionGainAdvanced),
     victoryPointsAdvanced(victoryPointsAdvanced),
