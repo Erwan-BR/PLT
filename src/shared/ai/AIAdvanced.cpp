aacf70c15eec51fb7ad9cbc3c3e1738b1e721295
Erwan <cuir-saunas.07@icloud.com>
2023-12-18 15:01:30 +0100

    **feature: Implementation of AI.dia**
    _Commit Description:_
    
    - Adding the dia of AI.
    - Adding empty methods in cpp file.
    
    [CR #050]

---
+++ src/shared/ai/AIAdvanced.cpp
@@ -0,0 +1,33 @@
+#include "./AIAdvanced.h"
+
+namespace ai
+{
+    AIAdvanced::AIAdvanced ()
+    {
+
+    }
+
+    AIAdvanced::AIAdvanced (std::string name, int id, sf::Texture* profilePicture)
+    {
+
+    }
+    AIAdvanced::~AIAdvanced ()
+    {
+
+    }
+
+    void AIAdvanced::AIChooseDraftingCard ()
+    {
+
+    }
+
+    void AIAdvanced::AIPlanification ()
+    {
+
+    }
+
+    void AIAdvanced::AIUseProducedResources ()
+    {
+
+    }
+};
---
+++ src/shared/ai/AIRandom.cpp
@@ -0,0 +1,33 @@
+#include "./AIRandom.h"
+
+namespace ai
+{
+    AIRandom::AIRandom ()
+    {
+
+    }
+
+    AIRandom::AIRandom (std::string name, int id, sf::Texture* profilePicture)
+    {
+
+    }
+    AIRandom::~AIRandom ()
+    {
+
+    }
+
+    void AIRandom::AIChooseDraftingCard ()
+    {
+
+    }
+
+    void AIRandom::AIPlanification ()
+    {
+
+    }
+
+    void AIRandom::AIUseProducedResources ()
+    {
+
+    }
+};
