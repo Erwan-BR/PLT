3542e075d909c53db8284576ff15e4342844c0f0
Erwan <cuir-saunas.07@icloud.com>
2023-12-18 17:16:58 +0100

    **feature: Implement two methods on random AI**
    _Commit Description:_
    
    - Implement the fact that the random AI choose a card from the draft phase.
    - Implement the fact that the AI make the planification.
    
    [CR #050]

--- src/shared/ai/AIRandom.cpp
+++ src/shared/ai/AIRandom.cpp
@@ -1,13 +1,18 @@
 #include "./AIRandom.h"
 
+#include <random>
+#include <chrono>
+
 namespace ai
 {
-    AIRandom::AIRandom ()
+    AIRandom::AIRandom () :
+        Player()
     {
 
     }
 
-    AIRandom::AIRandom (std::string name, int id, sf::Texture* profilePicture)
+    AIRandom::AIRandom (std::string name, int id, sf::Texture* profilePicture) :
+        Player(name, id, profilePicture)
     {
 
     }
@@ -18,16 +23,37 @@ namespace ai
 
     void AIRandom::AIChooseDraftingCard ()
     {
-
+        this->chooseDraftCard(0);
+        this->state = state::PlayerState::PENDING;
     }
 
     void AIRandom::AIPlanification ()
     {
+        for (int cardIndex = 7; 0 <= cardIndex; cardIndex++)
+        {
+            // Time-base seed to choose if the card is choose or not.
+			unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+			std::default_random_engine randomness(seed);
+            std::uniform_int_distribution<int> distribution(0, 1);
+            
+            // Generate a random boolean (false or true).
+            bool randomBoolean = distribution(randomness);
 
+            if (randomBoolean)
+            {
+                this->keepCard(cardIndex);
+            }
+            else
+            {
+                this->discardCard(cardIndex, true);
+            }
+        }
+        this->AIUseProducedResources();
+        this->state = state::PlayerState::PENDING;
     }
 
     void AIRandom::AIUseProducedResources ()
     {
-
+        
     }
 };
