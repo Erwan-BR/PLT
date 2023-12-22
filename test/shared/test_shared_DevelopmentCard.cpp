ad641158430c086eae276eeb8eb822d9c8a0904b
Erwan <cuir-saunas.07@icloud.com>
2023-12-22 14:04:41 +0100

    **feature: Update UT for DevelopmentCard**
    _Commit Description:_
    
    Update UT of DevelopmentCard by testing conversion JSON.
    
    [CR #053]

--- test/shared/test_shared_DevelopmentCard.cpp
+++ test/shared/test_shared_DevelopmentCard.cpp
@@ -8,7 +8,118 @@ using namespace ::state;
 
 BOOST_AUTO_TEST_CASE(firstDevelopmentCardTest)
 {
+  // Testing JSON conversion with some empty tables (Card is Reseau de transport)
   {
+    // Constructing a card that will be export to JSON
+
+    DevelopmentCard* cardToExport = new DevelopmentCard ("Reseau de transport",{},"../resources/img/Cards/Development_Cards/reseau_de_transport.png",new CardVictoryPoint{1, CardType::VEHICLE},CardType::STRUCTURE,2,{new ResourceToPay{ResourceType::MATERIAL, false}, new ResourceToPay{ResourceType::MATERIAL, false}, new ResourceToPay{ResourceType::MATERIAL, false}},{},ResourceType::MATERIAL);
+    
+    Json::Value jsonContent = cardToExport->toJSON();
+
+    /*
+    for (int i = 0; i < 7; i++)
+    {
+      std::cout << std::endl;
+    }
+    std::cout << jsonContent;
+    for (int i = 0; i < 7; i++)
+    {
+      std::cout << std::endl;
+    }*/
+
+    DevelopmentCard* cardFromImport = new DevelopmentCard(jsonContent);
+
+    BOOST_CHECK_EQUAL(cardFromImport->getName(), "Reseau de transport");
+    
+    std::vector<ResourceToProduce*> importedCardProduction = cardFromImport->getProductionGain();
+
+    BOOST_CHECK_EQUAL(importedCardProduction.size(), 0);
+
+    BOOST_CHECK_EQUAL(cardFromImport->getRelativePathToTexture(), "../resources/img/Cards/Development_Cards/reseau_de_transport.png");
+
+    CardVictoryPoint* cardPoints = cardFromImport->getVictoryPoints();
+    
+    BOOST_CHECK_EQUAL(cardPoints->numberOfPoints, 1);
+    BOOST_CHECK_EQUAL(cardPoints->cardOrResourceType, CardType::VEHICLE);
+
+    BOOST_CHECK_EQUAL(cardFromImport->getDiscardGain(), ResourceType::MATERIAL);
+    BOOST_CHECK_EQUAL(cardFromImport->getType(), CardType::STRUCTURE);
+
+    std::vector<ResourceToPay*> cardCostToBuild = cardFromImport->getCostToBuild();
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[0]->type, ResourceType::MATERIAL);
+    BOOST_CHECK_EQUAL(cardCostToBuild[0]->isPaid, false);
+    
+    BOOST_CHECK_EQUAL(cardCostToBuild[1]->type, ResourceType::MATERIAL);
+    BOOST_CHECK_EQUAL(cardCostToBuild[1]->isPaid, false);
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[2]->type, ResourceType::MATERIAL);
+    BOOST_CHECK_EQUAL(cardCostToBuild[2]->isPaid, false);
+
+    delete cardToExport;
+    delete cardFromImport;
+  }
+
+  // Testing JSON conversion with every no empty table. (Card is sattelites)
+  {
+    // Constructing a card that will be export to JSON
+    DevelopmentCard* cardToExport = new DevelopmentCard ("Satellites",{new ResourceToProduce{ResourceType::EXPLORATION, 2, CardType::NONETYPE}},"../resources/img/Cards/Development_Cards/satellites.png",new CardVictoryPoint{3, (int) CardType::NONETYPE},CardType::RESEARCH,1,{new ResourceToPay{ResourceType::ENERGY, false}, new ResourceToPay{ResourceType::ENERGY, false}, new ResourceToPay{ResourceType::SCIENCE, false}, new ResourceToPay{ResourceType::SCIENCE, false}, new ResourceToPay{ResourceType::SCIENCE, false}, new ResourceToPay{ResourceType::SCIENCE, false}},{ResourceType::COLONEL},ResourceType::EXPLORATION);    
+    
+    Json::Value jsonContent = cardToExport->toJSON();
+
+    /*for (int i = 0; i < 7; i++)
+    {
+      std::cout << std::endl;
+    }
+    std::cout << jsonContent;
+    for (int i = 0; i < 7; i++)
+    {
+      std::cout << std::endl;
+    }*/
+
+    DevelopmentCard* cardFromImport = new DevelopmentCard(jsonContent);
+
+    BOOST_CHECK_EQUAL(cardFromImport->getName(), "Satellites");
+    
+    std::vector<ResourceToProduce*> importedCardProduction = cardFromImport->getProductionGain();
+
+    BOOST_CHECK_EQUAL(importedCardProduction[0]->cardType, CardType::NONETYPE);
+    BOOST_CHECK_EQUAL(importedCardProduction[0]->quantity, 2);
+    BOOST_CHECK_EQUAL(importedCardProduction[0]->type, ResourceType::EXPLORATION);
+
+    BOOST_CHECK_EQUAL(cardFromImport->getRelativePathToTexture(), "../resources/img/Cards/Development_Cards/satellites.png");
+
+    CardVictoryPoint* cardPoints = cardFromImport->getVictoryPoints();
+    
+    BOOST_CHECK_EQUAL(cardPoints->numberOfPoints, 3);
+    BOOST_CHECK_EQUAL(cardPoints->cardOrResourceType, CardType::NONETYPE);
+
+    BOOST_CHECK_EQUAL(cardFromImport->getDiscardGain(), ResourceType::EXPLORATION);
+    BOOST_CHECK_EQUAL(cardFromImport->getType(), CardType::RESEARCH);
+
+    std::vector<ResourceToPay*> cardCostToBuild = cardFromImport->getCostToBuild();
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[0]->type, ResourceType::ENERGY);
+    BOOST_CHECK_EQUAL(cardCostToBuild[0]->isPaid, false);
+    
+    BOOST_CHECK_EQUAL(cardCostToBuild[1]->type, ResourceType::ENERGY);
+    BOOST_CHECK_EQUAL(cardCostToBuild[1]->isPaid, false);
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[2]->type, ResourceType::SCIENCE);
+    BOOST_CHECK_EQUAL(cardCostToBuild[2]->isPaid, false);
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[3]->type, ResourceType::SCIENCE);
+    BOOST_CHECK_EQUAL(cardCostToBuild[3]->isPaid, false);
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[4]->type, ResourceType::SCIENCE);
+    BOOST_CHECK_EQUAL(cardCostToBuild[4]->isPaid, false);
+
+    BOOST_CHECK_EQUAL(cardCostToBuild[5]->type, ResourceType::SCIENCE);
+    BOOST_CHECK_EQUAL(cardCostToBuild[5]->isPaid, false);
+  }
+
+
+  /*{
     // Testing the empty constructor of DevelopmentCard 
     DevelopmentCard* myFirstDevelopmentCard = new DevelopmentCard();
 
@@ -97,7 +208,7 @@ BOOST_AUTO_TEST_CASE(firstDevelopmentCardTest)
 
     // Delete pointers that won't be used anymore.
     mySecondDevelopmentCard->~DevelopmentCard();
-  }
+  }*/
 }
 
 /* vim: set sw=2 sts=2 et : */
