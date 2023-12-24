c10607dfc7d731829e5f4a67b547abbcfb7de14c
Onur <xblaze37@gmail.com>
2023-12-24 21:49:00 +0100

    **feature: UT EmpireCard**
    _Commit Description:_
    
    UT without json.
    
    [CR #053]

--- test/shared/test_shared_EmpireCard.cpp
+++ test/shared/test_shared_EmpireCard.cpp
@@ -6,53 +6,79 @@ using namespace ::state;
 
 BOOST_AUTO_TEST_CASE(firstEmpireCardTest)
 {
-  {
-    // Testing the empty constructor of EmpireCard 
-    EmpireCard* myFirstEmpireCard = new EmpireCard();
+    {
+      /*
+      // Elements to create for testing the full constructor.
+      ResourceToProduce* firstResourceToProduce = new ResourceToProduce{ResourceType::KRYSTALLIUM, 2, state::CardType::VEHICLE};
+      ResourceToProduce* secondResourceToProduce = new ResourceToProduce{ResourceType::GOLD, 3, state::CardType::PROJECT};
+
+      ResourceToProduce* firstAdvancedResourceToProduce = new ResourceToProduce{ResourceType::MATERIAL, 1, state::CardType::NONETYPE};
+      ResourceToProduce* secondAdvancedResourceToProduce = new ResourceToProduce{ResourceType::ENERGY, 3, state::CardType::PROJECT};
+
+      // Elements to pass to the constructor.
+      std::string name = "myName";
+
+      std::vector<ResourceToProduce*> productionGain;
+      productionGain.push_back(firstResourceToProduce);
+      productionGain.push_back(secondResourceToProduce);
+
+      sf::Texture* design = new sf::Texture();
+
+      CardVictoryPoint* victoryPoints = new CardVictoryPoint{1,state::CardType::NONETYPE};
+
+      std::vector<ResourceToProduce*> productionGainAdvanced;
+      productionGainAdvanced.push_back(firstAdvancedResourceToProduce);
+      productionGainAdvanced.push_back(secondAdvancedResourceToProduce);
+
+      CardVictoryPoint* victoryPointsAdvanced  = {};
+
+      EmpireLand empire = AZTEC;
+      */
+      // face B
+      ResourceToProduce* firstResourceToProduce = new ResourceToProduce{ResourceType::MATERIAL, 2, CardType::NONETYPE};
+      ResourceToProduce* secondResourceToProduce = new ResourceToProduce{ResourceType::ENERGY, 1, CardType::NONETYPE};
+      ResourceToProduce* thirdResourceToProduce = new ResourceToProduce{ResourceType::SCIENCE, 1, CardType::NONETYPE};
+      std::vector<ResourceToProduce*> productionGainB = {firstResourceToProduce,secondResourceToProduce, thirdResourceToProduce};		
+
+      bool isFaceA = true;
+      
+      // empire AFRICA
+      ResourceToProduce* firstResourceToProduceAFRICA = new ResourceToProduce{ResourceType::MATERIAL, 2, CardType::NONETYPE};
+      ResourceToProduce* secondResourceToProduceAFRICA = new ResourceToProduce{ResourceType::SCIENCE, 2, CardType::NONETYPE};
+      std::vector<ResourceToProduce*> productionGainAFRICA = {firstResourceToProduceAFRICA, secondResourceToProduceAFRICA};
+      sf::Texture* designAFRICA = new sf::Texture;
+      designAFRICA->loadFromFile("./resources/img/Cards/Empire_Face_A/Panafricaine.png");
+      sf::Texture* designAFRICA_FaceB = new sf::Texture;
+      designAFRICA_FaceB->loadFromFile("./resources/img/Cards/Empire_Face_B/Panafricaine.png");
+      CardVictoryPoint* victoryPointsAFRICA  = new CardVictoryPoint{2,CardType::RESEARCH};
     
-    // Calling methods of EmpireCard.
-    std::string EmpireCardToString = myFirstEmpireCard->toString();
+      // Testing the full constructor of EmpireCard.
+      EmpireCard* mySecondEmpireCard = new EmpireCard("AFRICA", productionGainAFRICA, designAFRICA, victoryPointsAFRICA, productionGainB, {}, AFRICA, designAFRICA_FaceB, isFaceA);
+      EmpireCard* myThirdEmpireCard = new EmpireCard("AFRICA", productionGainAFRICA, "./resources/img/Cards/Empire_Face_A/Panafricaine.png", victoryPointsAFRICA, productionGainB, {0}, AFRICA, "./resources/img/Cards/Empire_Face_B/Panafricaine.png", !isFaceA);
 
-    // Checking output of EmpireCard method.
-    BOOST_CHECK_EQUAL(EmpireCardToString, "");
 
-    // Delete pointers that won't be used anymore.
-    myFirstEmpireCard->~EmpireCard();
-  }
-  
-  {
-    // Elements to create for testing the full constructor.
-    ResourceToProduce* firstResourceToProduce = new ResourceToProduce{ResourceType::KRYSTALLIUM, 2, state::CardType::VEHICLE};
-    ResourceToProduce* secondResourceToProduce = new ResourceToProduce{ResourceType::GOLD, 3, state::CardType::PROJECT};
+      (void) mySecondEmpireCard->getDesignFaceB();
+      std::vector<ResourceToProduce*> productionGain = mySecondEmpireCard->getProductionGain();
+      BOOST_CHECK_EQUAL(productionGain[0],firstResourceToProduceAFRICA);
+      BOOST_CHECK_EQUAL(productionGain[1],secondResourceToProduceAFRICA);
 
-    ResourceToProduce* firstAdvancedResourceToProduce = new ResourceToProduce{ResourceType::MATERIAL, 1, state::CardType::NONETYPE};
-    ResourceToProduce* secondAdvancedResourceToProduce = new ResourceToProduce{ResourceType::ENERGY, 3, state::CardType::PROJECT};
+      productionGain = myThirdEmpireCard->getProductionGain();
+      BOOST_CHECK_EQUAL(productionGain[0],firstResourceToProduce);
+      BOOST_CHECK_EQUAL(productionGain[1],secondResourceToProduce);
+      BOOST_CHECK_EQUAL(productionGain[2],thirdResourceToProduce);
 
-    // Elements to pass to the constructor.
-    std::string name = "myName";
-
-    std::vector<ResourceToProduce*> productionGain;
-    productionGain.push_back(firstResourceToProduce);
-    productionGain.push_back(secondResourceToProduce);
-
-    sf::Texture design = sf::Texture();
-
-    CardVictoryPoint* victoryPoints = new CardVictoryPoint();
-
-    std::vector<ResourceToProduce*> productionGainAdvanced;
-    productionGainAdvanced.push_back(firstAdvancedResourceToProduce);
-    productionGainAdvanced.push_back(secondAdvancedResourceToProduce);
-
-    CardVictoryPoint* victoryPointsAdvanced  = new CardVictoryPoint();
-
-    EmpireLand empire = AZTEC;
+      CardVictoryPoint* victoryPoints = mySecondEmpireCard->getVictoryPoints();
+      //BOOST_CHECK_EQUAL(victoryPoints[0],2);
+      //BOOST_CHECK_EQUAL(victoryPoints[1],CardType::RESEARCH);
+      (void) mySecondEmpireCard->getVictoryPoints();
     
-    // Testing the full constructor of EmpireCard.
-    EmpireCard* mySecondEmpireCard = new EmpireCard(name, productionGain, design, victoryPoints, productionGainAdvanced, victoryPointsAdvanced, empire);
+      (void) myThirdEmpireCard->getRelativePathOfTextureFaceB();
+      (void) myThirdEmpireCard->getVictoryPoints();
 
-    // Delete pointers that won't be used anymore.
-    mySecondEmpireCard->~EmpireCard();
-  }
+      // Delete pointers that won't be used anymore.
+      delete mySecondEmpireCard;
+      //delete myThirdEmpireCard;
+    }
 }
 
 /* vim: set sw=2 sts=2 et : */
