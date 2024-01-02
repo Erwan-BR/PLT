0936fc8b4e3ee97fe8c0fcd604d12d6caf492740
Erwan <cuir-saunas.07@icloud.com>
2024-01-02 18:24:23 +0100

    **feature/refactor: Implement chooseColonel for random AI**
    _Commit Description:_
    
    - Implement choose colonel for random AI.
    - Refactor code by creating a function to use a single resource.
    
    [CR #050]

--- src/shared/ai/AIRandom.cpp
+++ src/shared/ai/AIRandom.cpp
@@ -5,13 +5,6 @@
 
 namespace ai
 {
-    /// @brief Empty construtor of AIRandom. Should never be used.
-    AIRandom::AIRandom () :
-        Player()
-    {
-
-    }
-
     /// @brief Full constructor of AIRandom, with important information inside.
     /// @param name Name of the AI.
     /// @param id ID of the AI. Should be negative for engine methods.
@@ -92,39 +85,66 @@ namespace ai
 
         std::shuffle(resourceToPlay.begin(), resourceToPlay.end(), randomness);
 
-        // Iterating among all resources that needs to be used.
+        // Iterating among all resources that needs to be use.
         for (state::ResourceType currentResource : resourceToPlay)
         {
             // Check if the resource is not playable, to send it directly into the empire.
-            if (!(this->isResourcePlayable(currentResource)))
+            if (this->isResourcePlayable(currentResource))
+            {
+                this->AIUseSingleResource(currentResource, numberOfCardsToBuild);
+            }
+            else
             {
                 this->sendResourceToEmpire(currentResource);
-                continue ;
-            }
-
-            // Here, the resource is playable. Choose a random card to try playing on it.
-            std::uniform_int_distribution<int> distribution(0, numberOfCardsToBuild);
-            
-            // Generate a random integer.
-            int randomCardPosition = distribution(randomness);
-
-            // As long as we are here, the resource is addable. We just have to iterate a certain time to add it on a card.
-            while(true)
-            {
-                // If the resource can be added on this card, do it and quit the loop to add the next resource.
-                if (this->toBuildCards[randomCardPosition]->isResourceAddable(currentResource))
-                {
-                    this->addResource(currentResource, randomCardPosition);
-                    break;
-                }
-                // Getting the next card position, to add it on another card. Re-intilization of the position if it exceed the size of the vector.
-                randomCardPosition ++;
-                if (numberOfCardsToBuild == randomCardPosition)
-                {
-                    randomCardPosition = 0;
-                }
             }
         }
         this->endProduction();
+    }
+
+    /// @brief Use a single resource to send it to a card. Called only if the resource is Playable.
+    void AIRandom::AIUseSingleResource (state::ResourceType resource, int numberOfCardsToBuild)
+    {
+        // Time-base seed to choose the card that will be try to receive resource first.
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine randomness(seed);
+
+        // Here, the resource is playable. Choose a random card to try playing on it.
+        std::uniform_int_distribution<int> distribution(0, numberOfCardsToBuild);
+        
+        // Generate a random integer.
+        int randomCardPosition = distribution(randomness);
+
+        // As long as we are here, the resource is addable. We just have to iterate a certain time to add it on a card.
+        // For loop to avoid any mistakes, but it could be a while(true) loop.
+        for (int index = 0; index < numberOfCardsToBuild; index++)
+        {
+            // If the resource can be added on this card, do it and quit the loop to add the next resource.
+            if (this->toBuildCards[randomCardPosition]->isResourceAddable(resource))
+            {
+                this->addResource(resource, randomCardPosition);
+                break;
+            }
+            // Getting the next card position, to add it on another card. Re-initialization of the position if it exceed the size of the vector.
+            randomCardPosition ++;
+            if (numberOfCardsToBuild == randomCardPosition)
+            {
+                randomCardPosition = 0;
+            }
+        }
+    }
+
+    /// @brief Choose randomly a token between colonel and financier.
+    /// @return Random boolean.
+    bool AIRandom::AIChooseColonelToken ()
+    {
+        // Time-base seed to choose if the card is choosed or not.
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine randomness(seed);
+        std::uniform_int_distribution<int> distribution(0, 1);
+        
+        // Generate a random integer.
+        int randomInt = distribution(randomness);
+        
+        return (bool) randomInt;
     }
 };
