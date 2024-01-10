87714fd3db259b2899b0388231a7b56f31bb67d0
Erwan <cuir-saunas.07@icloud.com>
2024-01-10 15:06:21 +0100

    **refactor: Move isADraftedCard in the command class**
    _Commit Description:_
    
    Move isADraftedCard in the command class
    
    [CR #077]

--- src/shared/engine/DiscardCard.cpp
+++ src/shared/engine/DiscardCard.cpp
@@ -7,10 +7,9 @@ namespace engine
     /// @param cardIndex Card that is kept during the drafting phase.
     /// @param isADraftedCard Indicate the origin of the card of the player.
     DiscardCard::DiscardCard (int playerIndex, int cardIndex, bool isADraftedCard) :
-        Command(CommandID::DISCARDCARD, playerIndex, cardIndex),
-        isADraftedCard(isADraftedCard)
+        Command(CommandID::DISCARDCARD, playerIndex, cardIndex)
     {
-
+        this->isADraftedCard = isADraftedCard;
     }
 
     /// @brief Destructor of the command. Should destruct anything because pointers are shared.
