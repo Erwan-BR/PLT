64df43b52f0f5eceda3ac36dc4ab7d2895485e77
Erwan <cuir-saunas.07@icloud.com>
2024-01-16 12:15:35 +0100

    **feature: Add a RsourceManager to stock textures**
    _Commit Description:_
    
    To avoid GPU / RAM problems, textures are saved in a static unordered map in a ResourceManager.
    
    [CR #078]

---
+++ src/constants/constants/ResourceManager.cpp
@@ -0,0 +1,21 @@
+#include "./ResourceManager.h"
+
+namespace constants
+{
+    // Initialize static member variables
+    std::unordered_map<std::string, sf::Texture> ResourceManager::textureMap;
+
+    const sf::Texture& ResourceManager::getTexture (const std::string& relativePath)
+    {
+        auto iterator = textureMap.find(relativePath);
+
+        // If the texture is not found, we have to load the value and store it.
+        if (iterator == textureMap.end())
+        {
+            textureMap[relativePath].loadFromFile(relativePath);
+            return textureMap[relativePath];
+        }
+        // Else, just return the value that is already in the map.
+        return iterator->second;
+    }
+}
